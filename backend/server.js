fd7a3ddd0f46510d6c42a5c7622f2c923f91308c
Padmajothi <padmajothi1603@gmail.com>
2025-01-21 00:49:10 -0500

    Analystics UI and coupon UI

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from "dotenv";
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 //routes
 import authRoutes from "./routes/auth.route.js";
@@ -17,6 +18,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolver();
+
 app.use(express.json( { limit: "10mb"}));
 app.use(cookieParser());
 
@@ -26,6 +29,14 @@ app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req,res) => {
+        res.sendFile(path.resolve(__dirnamee, "frontend", "dist", "index.html"));
+    })
+}
 
 app.listen(PORT, () => {
     console.log("Server running on http://localhost:" + PORT);
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import CategoryItem from '../components/CategoryItem';
+import { useProductStore } from '../stores/useProductStore';
+import FeaturedProducts from '../components/FeaturedProducts';
 
 
 const categories = [
@@ -13,6 +15,12 @@ const categories = [
 ]; 
 
 const HomePage = () => {
+  const { fetchFeaturedProducts, products, isLoading } = useProductStore();
+
+  useEffect(() => {
+    fetchFeaturedProducts();
+  },[fetchFeaturedProducts])
+
   return (
    <div className='relative min-h-screen text-white overflow-hidden'>
       <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 p-16">
@@ -27,6 +35,8 @@ const HomePage = () => {
             <CategoryItem category={category} key={category.name} />
           ))}
         </div>
+
+        {!isLoading && products.length > 0 && <FeaturedProducts featuredProducts={products} />}
       </div>
     </div>
   )
