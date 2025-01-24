0ad231edffeb5a88879f0094d727b92e8a17d2c2
Padmajothi <padmajothi1603@gmail.com>
2025-01-24 14:08:08 -0500

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -30,16 +30,15 @@ app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-    app.get("*", (req,res) => {
-        res.sendFile(path.resolve(__dirnamee, "frontend", "dist", "index.html"));
-    })
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
 }
 
 app.listen(PORT, () => {
-    console.log("Server running on http://localhost:" + PORT);
-
-    connectDB();
-})
+	console.log("Server is running on http://localhost:" + PORT);
+	connectDB();
+});
